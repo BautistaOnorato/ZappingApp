f0f9d6d44a650c9d29214f176afef5de9573a30d
BautistaOnorato <bautistaonorato27@gmail.com>
2023-09-27 21:49:37 -0300

    algunso estilos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ const Header = () => {
   const navigate = useNavigate()
   return (
     <header>
+      <img src="https://images.emojiterra.com/google/android-12l/512px/1f37f.png" alt="" />
       <button onClick={() => navigate("/")}>
         <HomeIcon />
       </button>
